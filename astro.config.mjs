c6d6c73325d1bd3172a8de893dd19f0e089ff1ad
Håvard Damm-Johnsen <havard.damm-johnsen@maths.ox.ac.uk>
2024-08-14 11:44:51 +0100

    fixed astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
-# import { defineConfig } from 'astro/config';
-
-# // https://astro.build/config
-# export default defineConfig({});
-
 import { defineConfig } from 'astro/config'
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://havarddj.github.io',
-  # base: 'webpage-2',
 })
